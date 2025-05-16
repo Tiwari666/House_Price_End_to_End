bc769d171eabaf4c1a8b4c2fbc083403cc1859b4
Tiwari666 <narendra76052@gmail.com>
2025-05-16 13:04:35 -0500

    Updated project name references to LR_BANKLOAN

--- setup.py
+++ setup.py
@@ -1,27 +1,27 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
 
-HYPEN_E_DOT='-e .'
-def get_requirements(file_path:str)->List[str]:
+HYPEN_E_DOT = '-e .'
+
+def get_requirements(file_path: str) -> List[str]:
     '''
-    this function will return the list of requirements
+    This function returns the list of requirements
     '''
-    requirements=[]
+    requirements = []
     with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
-    
+
     return requirements
 
 setup(
-name='House_Price_Prediction',
-version='0.0.1',
-author='Narendra',
-author_email='narendra76052@gmail.com',
-packages=find_packages(),
-install_requires=get_requirements('requirements.txt')
-
+    name='LR_BANKLOAN',  
+    version='0.0.1',
+    author='Narendra',
+    author_email='narendra76052@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
 )
